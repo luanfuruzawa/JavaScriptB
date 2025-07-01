78589c7df8a82229bb3cb30a6fe4867316ebaeec
luanfuruzawa <20241ctb0100006@estudantes.ifpr.edu.br>
2025-07-01 12:25:26 -0300

    aula de java script 01/07

---
+++ js/arquivo.js
@@ -0,0 +1,25 @@
+var global = "Seésciésci";
+console.log(global);
+function exemplo2() {
+    var local = "Aghaithemiéli";
+    console.log(local);
+}
+exemplo2();
+
+let x = 10;
+if(x>5){
+    let y = 20;
+    console.log("Valor de y: ", y);
+}else{
+    let y = 3;
+    console.log("Valor de y: ", y);
+}
+console.log(x);
+// console.log(y);
+
+const _constante = "Bhoidjty";
+console.log(_constante);  
+
+function saudacao() {
+    alert("Bem vindo ao nosso site 3");
+}
